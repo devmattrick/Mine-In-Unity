c3bc42bfb3a03657a0e9ec4d359ad3d3f3b1066e
theWildSushii <sushiiito123@gmail.com>
2017-06-17 07:17:53 -0600

    Fixed animations; Lighting configuration; Updated materials; Some other minor fixes

--- Assets/Scripts/DayNightCycle.cs
+++ Assets/Scripts/DayNightCycle.cs
@@ -3,23 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DayNightCycle : MonoBehaviour {
-
-	[Range(0, 24)]
-	public float hourOfDay = 9f;
+	
+	float hourOfDay = 9f;
 	public float timeSpeed = 0.6f;
 
 	// Update is called once per frame
 	void Update () {
-		Quaternion q = transform.rotation;
-		q.eulerAngles = new Vector3 (
-			( (hourOfDay / 24f) * 360f ) + 90f,
-			q.eulerAngles.y,
-			q.eulerAngles.z
-		);
-		transform.rotation = q;
-		hourOfDay += Time.deltaTime * Time.timeScale * timeSpeed;
-		if (hourOfDay > 24f) {
-			hourOfDay -= 24f;
+		transform.Rotate (Time.deltaTime * Time.timeScale * timeSpeed, 0f, 0f);
+	}
+
+	public float HourOfDay{
+		get {
+			return (transform.rotation.eulerAngles.x / 360f) * 24f;
+		}
+		set {
+			Quaternion q = transform.rotation;
+			q.eulerAngles = new Vector3 ((value / 24f) * 360f , q.eulerAngles.y, q.eulerAngles.z);
+			transform.rotation = q;
 		}
 	}
 
